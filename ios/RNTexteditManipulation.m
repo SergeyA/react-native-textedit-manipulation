59d35d725356f08b41be6c324e5a2c9ececf50d3
Sergey Alexeev <alekseevsa@gnivc.ru>
2022-08-11 01:10:40 +0300

    work on IOS implementation

--- ios/RNTexteditManipulation.m
+++ ios/RNTexteditManipulation.m
@@ -1,4 +1,5 @@
 
+#import <React/RCTSinglelineTextInputView.h>
 #import "RNTexteditManipulation.h"
 #import "RCTBridge+Private.h"
 #import "RCTUIManager.h"
@@ -14,13 +15,31 @@
 RCT_EXPORT_MODULE(TexteditManipulation)
 
 RCT_EXPORT_METHOD(insertText:(nonnull NSNumber *)reactTag withText:(NSString*)text) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
+    
+    RCTSinglelineTextInputView *reactView = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+    
+    UIView<RCTBackedTextInputViewProtocol> *view = reactView.backedTextInputView;
 
   [view replaceRange:view.selectedTextRange withText:text];
 }
 
+RCT_EXPORT_METHOD(setText:(nonnull NSNumber *)reactTag withText:(NSString*)text) {
+    
+    RCTSinglelineTextInputView *reactView = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+    
+    UIView<RCTBackedTextInputViewProtocol> *view = reactView.backedTextInputView;
+    
+    UITextPosition *firstCharacterPosition = [view beginningOfDocument];
+    UITextPosition *secondCharacterPosition = [view endOfDocument];
+    UITextRange *range = [view textRangeFromPosition:firstCharacterPosition toPosition:secondCharacterPosition];
+
+  [view replaceRange:range withText:@""];
+}
+
 RCT_EXPORT_METHOD(backSpace:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
+    RCTSinglelineTextInputView *reactView = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+    
+    UIView<RCTBackedTextInputViewProtocol> *view = reactView.backedTextInputView;
 
   UITextRange* range = view.selectedTextRange;
   if ([view comparePosition:range.start toPosition:range.end] == 0) {
@@ -30,7 +49,9 @@ RCT_EXPORT_METHOD(backSpace:(nonnull NSNumber *)reactTag) {
 }
 
 RCT_EXPORT_METHOD(doDelete:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
+    RCTSinglelineTextInputView *reactView = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+    
+    UIView<RCTBackedTextInputViewProtocol> *view = reactView.backedTextInputView;
 
   UITextRange* range = view.selectedTextRange;
   if ([view comparePosition:range.start toPosition:range.end] == 0) {
@@ -40,30 +61,37 @@ RCT_EXPORT_METHOD(doDelete:(nonnull NSNumber *)reactTag) {
 }
 
 RCT_EXPORT_METHOD(moveLeft:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
+    RCTSinglelineTextInputView *reactView = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+    
+    UIView<RCTBackedTextInputViewProtocol> *view = reactView.backedTextInputView;
 
   UITextRange* range = view.selectedTextRange;
   UITextPosition* position = range.start;
 
   if ([view comparePosition:range.start toPosition:range.end] == 0) {
-    position = [view positionFromPosition: position, offset: -1];
+      position = [view positionFromPosition: position offset: -1];
   }
 
-  view.selectedTextRange = [view textRangeFromPosition: position toPosition:position];
+  UITextRange* newRange = [view textRangeFromPosition: position toPosition:position];
+    
+  [view setSelectedTextRange:newRange notifyDelegate:false];
 }
 
 RCT_EXPORT_METHOD(moveRight:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
+    RCTSinglelineTextInputView *reactView = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+    
+    UIView<RCTBackedTextInputViewProtocol> *view = reactView.backedTextInputView;
 
   UITextRange* range = view.selectedTextRange;
   UITextPosition* position = range.end;
 
   if ([view comparePosition:range.start toPosition:range.end] == 0) {
-    position = [view positionFromPosition: position, offset: 1];
+      position = [view positionFromPosition: position offset: 1];
   }
 
-  view.selectedTextRange = [view textRangeFromPosition: position toPosition:position];
+  UITextRange* newRange = [view textRangeFromPosition: position toPosition:position];
+    
+  [view setSelectedTextRange:newRange notifyDelegate:false];
 }
 
 @end
-  
